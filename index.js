13d9f449be56d96dc9cc0e49753b54ae79ae431d
InnocentOgero <innocent.ogero@student.moringaschool.com>
2023-09-25 02:56:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(namesArray,eventName){
+    let messagesList=[];
+    for(let y=0;y<namesArray.length;y++){
+        let message=`Thank you, ${namesArray[y]}, for the wonderful ${eventName} gift!`;
+        messagesList.push(message);
+    }
+    return messagesList
+
+}
+function countDown(number){
+    while(number>-1){
+        console.log(number)
+        number=number-1
+        
+    }
+
+}
+
